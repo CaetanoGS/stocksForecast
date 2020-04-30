8b98b9a2fe5a405f3d3eddcc5fc097d44845a09b
Gustavo  Caetano <gustavo@Gustavo.mshome.net>
2020-04-30 16:49:04 +0200

    Back and Front integration 1.1

--- app/app/views.py
+++ app/app/views.py
@@ -4,6 +4,7 @@ from flask import jsonify
 import yfinance as yf
 from app import fillJson
 import json
+from datetime import datetime
 
 
 
@@ -22,18 +23,49 @@ def dashboard():
         initial_date = request.form['beginDate']
         period_select = request.form['period']
 
+        fdate_datetime = datetime.strptime(final_date,'%Y-%m-%d')
+        idate_datetime = datetime.strptime(initial_date, '%Y-%m-%d')
+
+        if(fdate_datetime < idate_datetime):
+            aux = final_date
+            final_date = initial_date
+            initial_date = aux
+            aux = fdate_datetime
+            fdate_datetime = idate_datetime
+            idate_datetime = aux
+        
+        dif = fdate_datetime - idate_datetime
+
+
+
         if(period_select == 'Daily'):
             op1 = 'Selected'
             op2 = ''
             op3 = ''
         elif(period_select == 'Weekly'):
-            op1 = ''
-            op2 = 'Selected'
-            op3 = ''
+            if(dif.days <= 5):
+                op1 = 'Selected'
+                op2 = ''
+                op3 = ''
+            else:
+                op1 = ''
+                op2 = 'Selected'
+                op3 = ''
         else:
-            op1 = ''
-            op2 = ''
-            op3 = 'Selected'
+            if(dif.days <= 5):
+                op1 = 'Selected'
+                op2 = ''
+                op3 = ''
+            elif (dif.days < 20):
+                op1 = ''
+                op2 = 'Selected'
+                op3 = ''
+            else:
+                op1 = ''
+                op2 = ''
+                op3 = 'Selected'
+
+
             
         return render_template('dashboard.html', name= company_name, fdate= final_date, idate= initial_date, option1=op1, option2=op2, option3=op3)
     else:
